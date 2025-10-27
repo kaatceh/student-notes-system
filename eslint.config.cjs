30469a61f73a75940c6084c31dc8c884bb128913
Katya Tereshchenko <kaatceh@gmail.com>
2025-10-27 11:30:00 +0200

    lab1: setup project tools

---
+++ eslint.config.cjs
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs'
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off'
+    }
+  }
+];
